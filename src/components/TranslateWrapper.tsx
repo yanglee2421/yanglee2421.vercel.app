12fa17c67d43bcd02b383fad65d30b76757742e8
yanglee2421 <yanglee2421@gmail.com>
2024-11-01 16:11:30 +0800

    refactor: rsc -> rcc

--- src/components/TranslateWrapper.tsx
+++ src/components/TranslateWrapper.tsx
@@ -1,26 +1,19 @@
+"use client";
+
 import React from "react";
 import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
 
 type Props = React.PropsWithChildren<{
   lang: string;
 }>;
 
-const langs = new Set<string>();
-langs.add("en");
-langs.add("zh");
+const langs = new Set(["en", "zh"]);
 
 export async function TranslateWrapper(props: Props) {
-  const cookie = await cookies();
-  const cookieLang = cookie.get("lang");
   const fallbackLang = "en";
 
   if (langs.has(props.lang)) {
     return props.children;
-  }
-
-  if (langs.has(cookieLang?.value || "")) {
-    return redirect(`/${cookieLang}`);
   }
 
   return redirect(`/${fallbackLang}`);
