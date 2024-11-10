b542c1ceda711f3711b706f283544ce1dad5ce6d
yanglee2421 <yanglee2421@gmail.com>
2024-11-11 01:21:36 +0800

    feat: add immer to zustand store

--- src/hooks/useLangStore.ts
+++ src/hooks/useLangStore.ts
@@ -1,25 +1,28 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+import { type WritableDraft } from "immer";
 
-type LangStore = {
+type LangStoreState = {
   lang: string;
-  set: {
-    (
-      partial:
-        | LangStore
-        | Partial<LangStore>
-        | ((state: LangStore) => LangStore | Partial<LangStore>),
-      replace?: false,
-    ): void;
-    (state: LangStore | ((state: LangStore) => LangStore), replace: true): void;
-  };
 };
 
-export const useLangStore = create(persist<LangStore>(
-  (set) => ({
+type LangStoreActions = {
+  set(
+    nextStateOrUpdater:
+      | LangStoreState
+      | Partial<LangStoreState>
+      | ((state: WritableDraft<LangStoreState>) => void),
+  ): void;
+};
+
+type LangStore = LangStoreState & LangStoreActions;
+
+export const useLangStore = create<LangStore>()(persist(
+  immer((set) => ({
     set,
     lang: "en",
-  }),
+  })),
   {
     name: "useLangStore",
     storage: createJSONStorage(() => globalThis.localStorage),
