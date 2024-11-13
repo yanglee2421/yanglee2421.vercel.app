0c73641ac5e173d1d8664194e6956f5c58a7e38f
yang <yanglee2421@gmail.com>
2024-11-13 22:00:13 +0800

    fix: oauth api

--- src/app/api/oauth/route.ts
+++ src/app/api/oauth/route.ts
@@ -1,5 +1,9 @@
-export async function POST(req: Request) {
-  const data = await req.json();
+import { URL } from "node:url";
+
+export async function Get(req: Request) {
+  const url = new URL(req.url);
+  const data = Object.fromEntries(url.searchParams.entries());
+
   console.log(data);
 
   return Response.json({ code: 200 });
