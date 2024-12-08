ee24f8c39e7061432650045e0c926b8527db9a47
yanglee2421 <yanglee2421@gmail.com>
2024-12-08 17:56:20 +0800

    feat add collapse nav feature

--- src/app/(doc_layout)/layout.tsx
+++ src/app/(doc_layout)/layout.tsx
@@ -3,18 +3,32 @@ import Link from "next/link";
 import React from "react";
 
 export default function Layout(props: React.PropsWithChildren) {
+  const showNav = false;
+
   return (
     <div>
-      <header className={classNames("fixed h-16 w-full border px-5 py-2")}>
+      <header
+        className={classNames("fixed h-16 w-full border px-5 py-2 shadow")}
+      >
         header
       </header>
-      <aside className="fixed h-dvh w-72 pt-16">
+      <aside
+        className={classNames(
+          "fixed h-dvh w-full pt-16 sm:max-w-72",
+          !showNav && "max-w-0",
+        )}
+      >
         <div className="h-full overflow-auto border">
           <div className=""></div>
           <nav>
             <ul className="divide-y *:px-5 *:py-2">
               <li className="">
-                <Link href="/">home</Link>
+                <Link
+                  href="/"
+                  className="text-base hover:text-indigo-500 hover:underline"
+                >
+                  home
+                </Link>
               </li>
               <li className="">
                 <Link
@@ -28,9 +42,14 @@ export default function Layout(props: React.PropsWithChildren) {
           </nav>
         </div>
       </aside>
-      <div className="flex min-h-dvh flex-col ps-72 pt-16">
+      <div
+        className={classNames(
+          "min-h-dvh flex-col pt-16 sm:flex sm:ps-72",
+          showNav ? "hidden" : "flex",
+        )}
+      >
         <main className="flex-grow">
-          <div className="border">{props.children}</div>
+          <div className="p-5">{props.children}</div>
         </main>
         <footer>
           <div className="border px-5 py-2">
