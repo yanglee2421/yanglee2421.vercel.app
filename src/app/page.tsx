5e32ce98f35345725be51c20fba335e63e1e2abd
yanglee2421 <yanglee2421@gmail.com>
2025-11-05 22:19:16 +0800

    feat: Add mode toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,37 @@
 import { Button } from "@/components/ui/button";
+import { cookies } from "next/headers";
 import Image from "next/image";
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
+const modeAction = async () => {
+  "use server";
+
+  const cookie = await cookies();
+  const mode = cookie.get("mode");
+  mode ? cookie.delete("mode") : cookie.set("mode", "1");
+  revalidatePath("/");
+};
+
+export default async function Home() {
+  const cookie = await cookies();
+  const mode = cookie.get("mode");
+
   return (
     <>
       <div>
-        <header className="fixed flex h-16 w-full items-center justify-start border-b border-slate-300 p-4 backdrop-blur-xs">
+        <header className="fixed z-10 flex h-16 w-full items-center justify-start border-b border-slate-300 p-4 backdrop-blur-xs">
           <h2 className="text-2xl">App Name</h2>
           <div className="ms-auto"></div>
-          <button className="cursor-pointer">menu</button>
+          <form action={modeAction} className="sm:hidden">
+            <button type="submit" className="cursor-pointer">
+              menu
+            </button>
+          </form>
         </header>
-        <aside className="fixed top-0 left-0 h-dvh w-64 pt-16">
+        <aside
+          className="fixed top-0 left-0 h-dvh w-full pt-16 data-[mode=true]:hidden sm:max-w-64 sm:data-mode:flex!"
+          data-mode={!!mode?.value}
+        >
           <nav className="size-full overflow-auto border-r border-slate-300">
             <ul className=" ">
               {Array.from({ length: 30 }, (_, index) => (
@@ -21,7 +42,7 @@ export default function Home() {
             </ul>
           </nav>
         </aside>
-        <div className="flex min-h-dvh flex-col border ps-64 pt-16">
+        <div className="flex min-h-dvh flex-col border pt-16 sm:ps-64">
           <main className="shrink-0 grow p-6">
             <Image
               className="dark:invert"
